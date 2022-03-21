409f064ad0b1b28658bf4a894e2332229bd1f48f
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-03-21 13:08:27 +0530

    feat-lesson 7

---
+++ lesson7/exercise1/script.js
@@ -0,0 +1,31 @@
+// const browserType = "Chrome";
+// console.log(browserType.slice(1, 4)); // "hro"
+// console.log(browserType.slice(2));//chrome
+
+// if (browserType.endsWith("rome")) {
+//   console.log("Found rome!");
+// } else {
+//   console.log("No rome here!");
+// }
+// if (browserType.startsWith("rome")) {
+//   console.log("Found rome!");
+// } else {
+//   console.log("No rome here!");
+// }
+
+// if (browserType.includes("rome")) {
+//   console.log("Found rome!");
+// } else {
+//   console.log("No rome here!");
+// }
+
+// const radData = 'My NaMe Is SkillCart';
+// console.log(radData.toLowerCase());
+// console.log(radData.toUpperCase());
+// const browserType = 'mozilla';
+// const updated = browserType.replace('moz','van');
+// console.log(updated);      // "vanilla"
+// console.log(browserType);  // "mozilla"
+let browserType = 'mozilla';
+browserType = browserType.replace('moz','van');
+console.log(browserType);  // "vanilla"
